3e83d8aa38ce3eb2df5d1d56349eab3b40c5068e
Unknown <devarshi.j.raval@gmail.com>
2018-04-08 16:37:51 +0530

    Updated with new apis

--- src/Pages/Dealers/Dealers.js
+++ src/Pages/Dealers/Dealers.js
@@ -22,6 +22,7 @@ class Dealers extends Component {
         this.setState({stateId: event.target.value});
 
         var endpoint = "https://farmrise-farmrise.1d35.starter-us-east-1.openshiftapps.com";
+        var endpoint = "http://localhost";
         fetch(endpoint + '/getDistList.php?SCode='+ event.target.value)
             .then(response => response.text())
             .then(data => {
@@ -35,6 +36,7 @@ class Dealers extends Component {
     handleChangeDist(event){
         this.setState({distId: event.target.value});
         var endpoint = "https://farmrise-farmrise.1d35.starter-us-east-1.openshiftapps.com";
+        var endpoint = "http://localhost";
         fetch(endpoint + '/getBlockList.php?SCode='+ this.state.stateId + '&DCode='+ event.target.value)
             .then(response => response.text())
             .then(data => {
@@ -50,6 +52,7 @@ class Dealers extends Component {
         console.log(this.state.stateId + this.state.distId + event.target.value);
 
         var endpoint = "https://farmrise-farmrise.1d35.starter-us-east-1.openshiftapps.com";
+        var endpoint = "http://localhost";
         fetch(endpoint + '/dealers.php?Type=P&SCode='+ this.state.stateId + '&DCode='+ this.state.distId + '&BCode=' + event.target.value)
             .then(response => response.text())
             .then(data => {
@@ -68,43 +71,43 @@ class Dealers extends Component {
                 <div className={this.state.hide}>
                     <label>Select State</label><br/>
                     <select id="stateSelect" onChange={this.handleChangeState} value={this.state.stateId}>
-                        <option value="0">--All--</option>
-                        <option value="31">A AND N ISLANDS</option>
-                        <option value="01">ANDHRA PRADESH</option>
-                        <option value="32">ARUNACHAL PRADESH</option>
-                        <option value="02">ASSAM</option>
-                        <option value="03">BIHAR</option>
-                        <option value="33">CHANDIGARH</option>
-                        <option value="23">CHHATTISGARH</option>
-                        <option value="34">DADRA AND NAGAR HAVELI</option>
-                        <option value="36">DAMAN AND DIU</option>
-                        <option value="35">DELHI</option>
-                        <option value="40">GOA</option>
-                        <option value="04">GUJARAT</option>
-                        <option value="05">HARYANA</option>
-                        <option value="06">HIMACHAL PRADESH</option>
-                        <option value="07">JAMMU AND KASHMIR</option>
-                        <option value="24">JHARKHAND</option>
-                        <option value="08">KARNATAKA</option>
-                        <option value="09">KERALA</option>
-                        <option value="37">LAKSHADWEEP</option>
-                        <option value="10">MADHYA PRADESH</option>
-                        <option value="11">MAHARASHTRA</option>
-                        <option value="12">MANIPUR</option>
-                        <option value="13">MEGHALAYA</option>
-                        <option value="38">MIZORAM</option>
-                        <option value="14">NAGALAND</option>
-                        <option value="15">ODISHA</option>
-                        <option value="39">PUDUCHERRY</option>
-                        <option value="16">PUNJAB</option>
-                        <option value="17">RAJASTHAN</option>
-                        <option value="22">SIKKIM</option>
-                        <option value="18">TAMIL NADU</option>
-                        <option value="26">TELANGANA</option>
-                        <option value="19">TRIPURA</option>
-                        <option value="20">UTTAR PRADESH</option>
-                        <option value="25">UTTARAKHAND</option>
-                        <option value="21">WEST BENGAL</option>
+                        <option selected="selected" value="0">--All--</option>
+                        <option value="35">ANDAMAN AND NICOBAR ISLANDS</option>
+                        <option value="28">ANDHRA PRADESH</option>
+                        <option value="12">ARUNACHAL PRADESH</option>
+                        <option value="18">ASSAM</option>
+                        <option value="10">BIHAR</option>
+                        <option value="4">CHANDIGARH</option>
+                        <option value="22">CHHATTISGARH</option>
+                        <option value="26">DADRA AND NAGAR HAVELI</option>
+                        <option value="25">DAMAN AND DIU</option>
+                        <option value="7">DELHI</option>
+                        <option value="30">GOA</option>
+                        <option value="24">GUJARAT</option>
+                        <option value="6">HARYANA</option>
+                        <option value="2">HIMACHAL PRADESH</option>
+                        <option value="1">JAMMU AND KASHMIR</option>
+                        <option value="20">JHARKHAND</option>
+                        <option value="29">KARNATAKA</option>
+                        <option value="32">KERALA</option>
+                        <option value="31">LAKSHADWEEP</option>
+                        <option value="23">MADHYA PRADESH</option>
+                        <option value="27">MAHARASHTRA</option>
+                        <option value="14">MANIPUR</option>
+                        <option value="17">MEGHALAYA</option>
+                        <option value="15">MIZORAM</option>
+                        <option value="13">NAGALAND</option>
+                        <option value="21">ODISHA</option>
+                        <option value="34">PUDUCHERRY</option>
+                        <option value="3">PUNJAB</option>
+                        <option value="8">RAJASTHAN</option>
+                        <option value="11">SIKKIM</option>
+                        <option value="33">TAMIL NADU</option>
+                        <option value="36">TELANGANA</option>
+                        <option value="16">TRIPURA</option>
+                        <option value="5">UTTAR PRADESH</option>
+                        <option value="9">UTTARAKHAND</option>
+                        <option value="19">WEST BENGAL</option>
                     </select>
                     <br/>
                     <br/>
